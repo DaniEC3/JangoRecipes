4a1b3f944a7264d1c2eb312691656bfecb9f4f56
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-11-18 18:59:30 -0500

    Refactor Recipe model and remove users app
    Updated the Recipe model to use 'name' instead of 'title', added a user foreign key, and introduced a difficulty_level field with choices. Removed the users app and its related files, consolidating user management to Django's built-in User model.

--- src/jangorecipes/settings.py
+++ src/jangorecipes/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #recipe-related apps
-    'recipes',
+    'recipes'
 ]
 
 MIDDLEWARE = [
--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     ingredients = models.TextField()
+    DIFFICULTY_CHOICES = [
+       ('easy', 'Easy'),
+       ('medium', 'Medium'),
+       ('intermediate', 'Intermediate'),
+       ('hard', 'Hard'),
+   ]
+    difficulty_level = models.CharField(max_length=50, choices=DIFFICULTY_CHOICES)
     instructions = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
-        return self.title
+        return self.name  
