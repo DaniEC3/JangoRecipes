1fb2ea9c0167ac090f2e9d94dc12bbdbf1af0c7d
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-11-25 13:59:59 -0500

    Add recipe detail view and template
    Introduced a recipe detail page with a new template and view. Updated URLs to include a route for viewing individual recipes. Also added static admin and recipe image files.

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home
+from .views import details
 
 app_name = 'recipes'
 urlpatterns = [
    path('', home),
+   path('recipe/<int:id>', details),
 ]  
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
 # Create your views here.
 def home(request):
    return render(request, 'recipes/home.html')
+
+def details(request, id):
+   recipe = get_object_or_404(Recipe, pk=id)
+   return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
