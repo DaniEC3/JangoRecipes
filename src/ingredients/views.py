da03bff285d01be0d3b87dbc6584cd28a1b738a6
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-12-05 17:54:39 -0500

    Add ingredient and recipe images, tests for Ingredient queryset
    Added image files for ingredients and recipes to the media directory. Implemented comprehensive tests for Ingredient model queryset operations in tests.py. Updated views.py to import Ingredient and refactored form variable names. Removed db.sqlite3 from .gitignore and added the database file.

--- src/ingredients/views.py
+++ src/ingredients/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
 from .forms import IngredientSearchForm
+from .models import Ingredient
 
 # Create your views here.
 @login_required
@@ -13,14 +14,32 @@ def ingredient_search(request):
     form = IngredientSearchForm(request.POST or None)
     # Create the instance from forms
     if request.method == 'POST':
-        recipe_name = request.POST.get('recipe_name')
+        ingredient_name = request.POST.get('ingredient_name')
         chart_type = request.POST.get('chart_type')
-        print(recipe_name)
-        print(chart_type)
 
+        # print ('Exploring querysets:')
+        # # .objects - Django's model manager (gives you access to database queries)
+        # print ('Case 1: Output of Ingredient.objects.all()')
+        # ingredient = Ingredient.objects.all()
+        # print(ingredient)
+
+        # print ('Case 2: Output of Ingredient.objects.filter(name__icontains=ingredient_name)')
+        # ingredient = Ingredient.objects.filter(name__icontains=ingredient_name)
+        # print (ingredient)
+
+        # print ('Case 3: Output of ingredient.values')
+        # print (ingredient.values())
+
+        # print ('Case 4: Output of ingredient.values_list()')
+        # print (ingredient.values_list())
+
+        # print ('Case 5: Output of Ingredient.objects.get(id=1)')
+        # obj = Ingredient.objects.get(id=1)
+        # print (obj)
     #pack up data to be sent to template in the context dictionary
     context={
            'form': form,
+        #    'ingredient': ingredient,
     }
     #load the sales/record.html page using the data that you just prepared
     return render(request, 'ingredients/ingredient_search.html', context)
