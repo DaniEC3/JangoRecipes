cadd5cf33bdf6d32a629b7141991682d9e63baec
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-12-06 14:00:26 -0500

    Add chart rendering to ingredient search
    Introduces chart generation using matplotlib and pandas for ingredient search results. Adds a utility module for chart creation, updates the view to process and display charts and dataframes, and modifies the template to show the chart and data table if available.

--- src/ingredients/views.py
+++ src/ingredients/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
 from .forms import IngredientSearchForm
 from .models import Ingredient
+import pandas as pd
+from pandas import DataFrame
+from .utils import get_chart
 
 # Create your views here.
 @login_required
@@ -12,34 +15,22 @@ def IngredientsHome(request):
 @login_required
 def ingredient_search(request):
     form = IngredientSearchForm(request.POST or None)
+    ingredients_df = None     #initialize dataframe to None
+    chart = None
     # Create the instance from forms
     if request.method == 'POST':
         ingredient_name = request.POST.get('ingredient_name')
         chart_type = request.POST.get('chart_type')
 
-        # print ('Exploring querysets:')
-        # # .objects - Django's model manager (gives you access to database queries)
-        # print ('Case 1: Output of Ingredient.objects.all()')
-        # ingredient = Ingredient.objects.all()
-        # print(ingredient)
-
-        # print ('Case 2: Output of Ingredient.objects.filter(name__icontains=ingredient_name)')
-        # ingredient = Ingredient.objects.filter(name__icontains=ingredient_name)
-        # print (ingredient)
-
-        # print ('Case 3: Output of ingredient.values')
-        # print (ingredient.values())
-
-        # print ('Case 4: Output of ingredient.values_list()')
-        # print (ingredient.values_list())
-
-        # print ('Case 5: Output of Ingredient.objects.get(id=1)')
-        # obj = Ingredient.objects.get(id=1)
-        # print (obj)
-    #pack up data to be sent to template in the context dictionary
+        qs = Ingredient.objects.filter(name__icontains=ingredient_name)
+        if qs.exists():
+            ingredients_df = DataFrame(qs.values())
+            chart=get_chart(chart_type, ingredients_df, labels=ingredients_df['quantity'].values)
+            ingredients_df = ingredients_df.to_html()
     context={
            'form': form,
-        #    'ingredient': ingredient,
+           'ingredients_df': ingredients_df,
+           'chart': chart
     }
     #load the sales/record.html page using the data that you just prepared
     return render(request, 'ingredients/ingredient_search.html', context)
